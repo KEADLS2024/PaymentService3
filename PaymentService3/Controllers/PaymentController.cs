6b2b5c9e2c9eebc2e68e831293ccc77ac3ecd2a3
FrederikkeGylstorff1 <frederikke@sneugle.dk>
2024-05-13 11:08:41 +0200

    Add project files.

---
+++ PaymentService3/Controllers/PaymentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using RabbitMQ.Client;
+using Stripe;
+using Stripe.Checkout;
+using PaymentService3.Models;
+
+namespace PaymentService3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : Controller
+    {
+        private readonly StripeSettings _stripeConfig;
+        public PaymentController(IConfiguration configuration)
+        {
+            _stripeConfig = configuration.GetSection("StripeSettings").Get<StripeSettings>();
+            StripeConfiguration.ApiKey = _stripeConfig.SecretKey;
+        }
+
+        [HttpPost]
+        [Route("create-checkout-session")]
+        public async Task<IActionResult> CreateCheckoutSession([FromBody] CreateCheckoutSessionRequest request)
+        {
+            try
+            {
+                var options = new SessionCreateOptions
+                {
+                    PaymentMethodTypes = new List<string>
+                    {
+                        "card",
+                    },
+                    LineItems = new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                Currency = "usd",
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = request.ProductName,
+                                },
+                                UnitAmount = request.ProductPrice,
+                            },
+                            Quantity = request.Quantity,
+                        },
+                    },
+                    Mode = "payment",
+                    SuccessUrl = "https://example.com/success",
+                    CancelUrl = "https://example.com/cancel",
+                };
+
+                var service = new SessionService();
+                var session = await service.CreateAsync(options);
+                return Ok(new { sessionId = session.Id });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+
+}
+
+
---
+++ PaymentService3/Models/CreateCheckoutSessionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentService3.Models
+{
+    public class CreateCheckoutSessionRequest
+    {
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
